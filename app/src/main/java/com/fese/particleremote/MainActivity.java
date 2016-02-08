4d140d13ef4624ac48c49cb0aa0a6a36f3820390
Fese <fese_fabi@gmx.de>
2016-02-08 22:45:46 +0100

    commit different code

--- app/src/main/java/com/fese/particleremote/MainActivity.java
+++ app/src/main/java/com/fese/particleremote/MainActivity.java
@@ -43,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
     private io.particle.android.sdk.cloud.ParticleDevice selectedDevice;
 
     //check if this appear after commit
+    //successfully after merge
 
 
     @Override
