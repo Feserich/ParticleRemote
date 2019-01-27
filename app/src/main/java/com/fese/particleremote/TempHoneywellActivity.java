8f56605a23103713ead643a4696b0175ff71f2ad
Fese <fabian.fesenmayr@gmail.com>
2019-01-27 20:37:02 +0100

    added function to set temperature in the future

--- app/src/main/java/com/fese/particleremote/TempHoneywellActivity.java
+++ app/src/main/java/com/fese/particleremote/TempHoneywellActivity.java
@@ -1,6 +1,10 @@
 package com.fese.particleremote;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.icu.util.Calendar;
+import android.icu.util.GregorianCalendar;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.design.widget.Snackbar;
@@ -14,9 +18,11 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.RelativeLayout;
 import android.widget.TextSwitcher;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.ViewSwitcher;
 
 import net.steamcrafted.loadtoast.LoadToast;
@@ -32,6 +38,7 @@ import io.particle.android.sdk.cloud.ParticleCloudSDK;
 import io.particle.android.sdk.cloud.ParticleDevice;
 import io.particle.android.sdk.utils.Async;
 
+import static java.lang.Math.toIntExact;
 
 
 public class TempHoneywellActivity extends AppCompatActivity {
@@ -69,6 +76,7 @@ public class TempHoneywellActivity extends AppCompatActivity {
         Button btn_setTempOff = findViewById(R.id.btn_tempOff);
         Button btn_setTempComfort = findViewById(R.id.btn_tempComfort);
         Button btn_setTempNight = findViewById(R.id.btn_tempNight);
+        Button btn_setFutureTemp = findViewById(R.id.btn_setFutureTemp);
 
         setTempToast = new LoadToast(this);
         setTempToast.setText(getString(R.string.setting_temperature_toast_text));
@@ -78,6 +86,7 @@ public class TempHoneywellActivity extends AppCompatActivity {
         btn_setTempOff.setOnClickListener(setTempOffOnClick);
         btn_setTempComfort.setOnClickListener(setTempComfortOnClick);
         btn_setTempNight.setOnClickListener(setTempNightOnClick);
+        btn_setFutureTemp.setOnClickListener(openFutureTempTimePickerOnClick);
 
 
 
@@ -220,6 +229,98 @@ public class TempHoneywellActivity extends AppCompatActivity {
 
     }
 
+    private void setFutureTemperature(String targetFutureTemp, long diffMiliseconds){
+
+        int minutesTillHeatingStart;
+        String delimeter = ";";
+
+        minutesTillHeatingStart = (int)Math.ceil((double)diffMiliseconds / 60000);
+
+        final String command = targetFutureTemp + delimeter
+                + String.valueOf(minutesTillHeatingStart) + delimeter;
+
+        if (myParticleDevice != null){
+
+            setTempToast.show();
+            Async.executeAsync(myParticleDevice, new Async.ApiWork<io.particle.android.sdk.cloud.ParticleDevice, Integer>() {
+
+                @Override
+                public Integer callApi(io.particle.android.sdk.cloud.ParticleDevice particleDevice) throws ParticleCloudException, IOException {
+                    Integer success = 0;
+
+                    //the commands in functionCommandList will be executed by the Particle device
+                    ArrayList<String> functionCommandList = new ArrayList<>();
+                    functionCommandList.add(command);
+
+                    try {
+                        success = particleDevice.callFunction("setTempFut",functionCommandList);
+
+                    } catch (io.particle.android.sdk.cloud.ParticleDevice.FunctionDoesNotExistException e) {
+                        Snackbar snackbarError = Snackbar
+                                .make(viewHoneywell, e.getMessage(), Snackbar.LENGTH_LONG);
+                        snackbarError.show();
+
+                    }
+
+                    return success;
+                }
+
+
+                public void onSuccess(Integer returnValue) {
+
+                    switch (returnValue){
+                        case 1:
+                            setTempToast.success();
+                            break;
+                        case -1:
+                            Snackbar snackbarError1 = Snackbar
+                                    .make(viewHoneywell, getString(R.string.wrong_response), Snackbar.LENGTH_LONG);
+                            snackbarError1.show();
+                            setTempToast.error();
+                            break;
+                        case -2:
+                            Snackbar snackbarError2 = Snackbar
+                                    .make(viewHoneywell, getString(R.string.read_buffer_overflow), Snackbar.LENGTH_LONG);
+                            snackbarError2.show();
+                            setTempToast.error();
+                            break;
+                        case -3:
+                            Snackbar snackbarError3 = Snackbar
+                                    .make(viewHoneywell, getString(R.string.time_out), Snackbar.LENGTH_LONG);
+                            snackbarError3.show();
+                            setTempToast.error();
+                            break;
+                        default:
+                            setTempToast.error();
+                            break;
+                    }
+
+                    //recyclerView.getAdapter().notifyDataSetChanged();
+                }
+
+                public void onFailure(ParticleCloudException e) {
+                    Log.e("SOME_TAG", e.getBestMessage());
+                    Snackbar snackbarError = Snackbar
+                            .make(viewHoneywell, e.getBestMessage(), Snackbar.LENGTH_LONG);
+                    snackbarError.show();
+                    setTempToast.error();
+
+                }
+
+            });
+        }
+        else {
+            Snackbar snackbarWait = Snackbar
+                    .make(viewHoneywell, "Wait a sec!", Snackbar.LENGTH_LONG);
+            snackbarWait.show();
+        }
+
+
+
+
+
+    }
+
     private void getParticleDeviceInstance(){
 
         //get the ID of the selected Particle Device from the MainActivity
@@ -288,6 +389,56 @@ public class TempHoneywellActivity extends AppCompatActivity {
             setTemperature(comfortValue + "0");               //multiple 10 for RAW value == add a zero
             setTextSwitchTemperature(comfortValue);
             discreteSeekBarTargetTemp.setProgress(Integer.valueOf(comfortValue));
+
+        }
+
+    };
+
+    View.OnClickListener openFutureTempTimePickerOnClick = new View.OnClickListener(){
+        @Override
+        public void onClick(View v) {
+
+            Calendar mcurrentDate = Calendar.getInstance();
+            final int mYear = mcurrentDate.get(Calendar.YEAR);
+            final int mMonth = mcurrentDate.get(Calendar.MONTH);
+            final int mDay = mcurrentDate.get(Calendar.DAY_OF_MONTH);
+            final int hour = mcurrentDate.get(Calendar.HOUR_OF_DAY);
+            final int minute = mcurrentDate.get(Calendar.MINUTE);
+
+            final long currentUnixTimestamp = mcurrentDate.getTimeInMillis();
+
+            DatePickerDialog mDatePicker;
+            mDatePicker = new DatePickerDialog(TempHoneywellActivity.this, new DatePickerDialog.OnDateSetListener() {
+                public void onDateSet(DatePicker datepicker, final int selectedyear, final int selectedmonth, final int selectedday) {
+
+                    TimePickerDialog mTimePicker;
+                    mTimePicker = new TimePickerDialog(TempHoneywellActivity.this, new TimePickerDialog.OnTimeSetListener() {
+                        @Override
+                        public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+                            String targetTempStr;
+
+                            if (targetTemp.equals(getString(R.string.LOWEST_TEMPERATURE_SELECTION_VALUE))) {
+                                targetTempStr = getString(R.string.HONEYWELL_OFF_VALUE);    //value to set Honeywell to OFF
+                            }
+                            else {
+                                targetTempStr = targetTemp + "0";                           //multiple 10 for RAW value == add a zero
+
+                            }
+                            Calendar calenderInstance = new GregorianCalendar(selectedyear,
+                                    selectedmonth, selectedday, selectedHour, selectedMinute);
+                            long selectedUnixTimeStamp = calenderInstance.getTimeInMillis();
+                            setFutureTemperature(targetTempStr, selectedUnixTimeStamp - currentUnixTimestamp);
+                        }
+                    }, hour, minute, true);//Yes 24 hour time
+                    mTimePicker.setTitle("Select Heat Start Time");
+                    mTimePicker.show();
+
+                }
+            }, mYear, mMonth, mDay);
+            mDatePicker.setTitle("Select Heat Start Date");
+            mDatePicker.getDatePicker().setMinDate(System.currentTimeMillis() - 1000);
+            mDatePicker.getDatePicker().setMaxDate(System.currentTimeMillis() + 7 * 24 * 3600 * 1000);
+            mDatePicker.show();
 
         }
 
